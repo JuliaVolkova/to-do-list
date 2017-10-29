2bc979de7e6e1d13250cafde8d5713df1376bf2f
Julia Volkova <julia.volkova789@gmail.com>
2017-10-29 14:31:18 +0300

    feat: Add list rendering, make module

--- js/script.js
+++ js/script.js
@@ -1,85 +1,111 @@
 'use strict';
 
-var addButton = document.querySelector('.add');
-var taskInput = document.querySelector('.new-item');
-var deadlineDate = document.querySelector('.deadline');
-var taskList = document.querySelector('.tasks');
-var removeButton = document.querySelector('.remove');
+var module = (function () {
 
-
-/**
- * @param {String} text
- * @param {String} deadline // Date??
- * @param {Boolean} done
- * @constructor
- */
-function Task(text, deadline, done) {
-    this.text = text;
-    this.deadline = deadline;
-    this.done = done;
-    this.element = document.querySelector('#task-template').content.cloneNode(true);
+    var addButton = document.querySelector('.add');
+    var taskInput = document.querySelector('.new-item');
+    var deadlineDate = document.querySelector('.deadline');
+    var removeButton = document.querySelector('.remove');
+    var page;
 
     /**
-     * @return {Node} Отрисовывает эелемент на странице
+     * @param {String} text
+     * @param {Date} deadline
+     * @param {Boolean} done
+     * @constructor
      */
-    Task.prototype.render = function () {
-        this.element.querySelector('.task-text').textContent = this.text;
-        this.element.querySelector('.date').textContent = this.deadline;
-        return this.element;
-    };
-}
+    function Task(text, deadline, done) {
+        this.text = text;
+        this.deadline = deadline;
+        this.done = done;
+        this.template = document.querySelector('#task-template');
 
-/**
- *
- * @param {Array} tasks
- * @constructor
- */
-function List (tasks) {
-    this.tasks = tasks;
+        /**
+         * @return {Node} Отрисовывает эелемент на странице
+         */
+        Task.prototype.render = function () {
+            var element = this.template.content.cloneNode(true);
+            element.querySelector('.task-text').textContent = this.text;
+            element.querySelector('.date').textContent = this.deadline.toDateString();
+            return element;
+        };
+    }
 
     /**
-     * @return {Array} возвращает массив элементов с текстами заданий
+     * @param {Array} tasks
+     * @constructor
      */
-    List.prototype.add = function (item) {
-        this.tasks.push(item);
-    };
+    function List(tasks) {
+        this.tasks = tasks;
+        this.template = document.querySelector('#list-template');
 
-    /**
-     * @return {Array} удаляет элемент
-     */
-    List.prototype.remove = function (item) {
+        /**
+         * @return {Array} возвращает массив элементов с текстами заданий
+         */
+        List.prototype.add = function (item) {
+            this.tasks.push(item);
+            this.render();
+        };
 
-        this.tasks.splice(this.tasks.indexOf(item), 1);
-    };
+        /**
+         * @return {Array} удаляет элемент
+         */
+        List.prototype.remove = function (item) {
+            this.tasks.splice(this.tasks.indexOf(item), 1);
+        };
 
-    /**
-     * @return {Node} позволяет редактировать текст
-     */
-    List.prototype.edit = function () {
+        /**
+         * @return {Node} позволяет редактировать текст
+         */
+        List.prototype.edit = function () {
 
-       // TODO 20.10.17 implement
-    };
+            // TODO 20.10.17 implement
+        };
 
-    /**
-     * @return {Array} возвращает массив элементов заданий, отфильтрованных по дате
-     */
-    List.prototype.filter = function () {
-        // TODO 20.10.17 implement
-    };
-}
+        /**
+         * @return {Array} возвращает массив элементов заданий, отфильтрованных по дате
+         */
+        List.prototype.filter = function () {
+            // TODO 20.10.17 implement
+        };
 
-var list = new List([]);
+        List.prototype.render = function () {
+            var element = this.template.content.querySelector('.tasks').cloneNode(true);
+            var fragment = document.createDocumentFragment();
+            tasks.forEach(function (task) {
+                fragment.appendChild(task.render())
+            });
+            element.appendChild(fragment);
+            var oldChild = page.querySelector('.tasks');
+            if (oldChild) {
+                page.removeChild(oldChild);
+            }
+            page.appendChild(element);
+        }
+    }
 
-function addNewTask () {
+    var list = new List([]);
 
-    var task = new Task(taskInput.value, deadlineDate.value, false);
-    list.add(task);
-    taskList.appendChild(task.render());
-}
+    function addNewTask() {
+        list.add(new Task(taskInput.value, new Date(deadlineDate.value), false));
+    }
 
-function removeTask () {
+    function removeTask() {
 
-}
+    }
 
-addButton.addEventListener('click', addNewTask);
-//removeButton.addEventListener('click', removeTask);
+    return {
+        /**
+         * @param {HTMLElement} container
+         */
+        init: function (container) {
+            // removeButton.addEventListener('click', removeTask);
+            addButton.addEventListener('click', addNewTask);
+            page = container;
+            list.render();
+
+        }
+    }
+})();
+
+module.init(document.querySelector('.container'));
